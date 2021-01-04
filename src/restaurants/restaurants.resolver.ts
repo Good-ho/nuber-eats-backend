4e15123bda34e7685907ba2a6140b6763c6b13f7
good-ho <whdxo100@gmail.com>
2021-01-04 21:12:59 +0900

    TypeORM Setup

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,19 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './restaurants.entity';
 
 @Resolver((of) => Restaurant)
 export class RestaurantResolver {
-  @Query((returns) => Restaurant)
-  myRestaurant() {
+  @Query((returns) => [Restaurant])
+  myRestaurant(@Args('vegan') vegan: boolean): Restaurant[] {
+    return [];
+  }
+  @Mutation((returns) => Boolean)
+  createRestaurants(
+    @Args('name') name: string,
+    @Args('isVegan') isVegan: boolean,
+    @Args('address') address: string,
+    @Args('ownerName') ownerName: string,
+  ): boolean {
     return true;
   }
 }
