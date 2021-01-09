219ea55510f6848429f16db09f6a920aa8d063c9
good-ho <whdxo100@gmail.com>
2021-01-09 20:16:42 +0900

    Create Account Mutation part Three 1. 이번 강의의 핵심은 go 언어처럼 error를 내가 직접 handling하는 것이다. 2. service 코드를 보면, user가 exists한 경우 "already exists" string 을 return 한다. 3. error를 받은 resolver는 우리가 정의한  dtooutput에 따라 custom error 및 status 를 return 한다!!

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -11,20 +11,23 @@ export class UsersService {
   ) {}
 
   //createAccount(createAccountInput:CreateAccountInput){
-  async createAccount({ email, password, role }: CreateAccountInput) {
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
     // 1. check that email does not exists
     // 2. create the user && hash the password
     try {
       const exists = await this.user.findOne({ email });
       if (exists) {
         // make error
-        return;
+        return 'already exists';
       }
       await this.user.save(this.user.create({ email, password, role }));
-      return true;
     } catch (e) {
       //make error
-      return;
+      return 'can not create user';
     }
   }
 }
