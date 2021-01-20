60dc855742ff94525efd815a81f1bcc94a6ced09
good-ho <whdxo100@gmail.com>
2021-01-20 19:10:51 +0900

    Mocking 1. jest path error - 처음 시작하면 path error가 나온다. - 기본적으로 우리가 import시 작성한 path는 src/~ 이렇게 시작하는데 이 path를 못알아보는 것. 따라서 package.json에 path를 알아먹도록 정규식 추가. (src로 시작하는 놈은 roodir-src부터 보게하는것)
    2. 두번째 에러.
    - 두번째에러는 dependency 문제다.
    - user service에는 user repository, verification repository, jwtService, MailService등을 사용하는데 이것역시 test module에서 provide설정 즉 dependency설정해야한다는 소리.
    - 우리는 typeorm에서 repository를 읽어 오지는 않을것이다. - muck repository를 제공할 것이다. mock이란 가짜함수다.
    
    - 왜 mock을 사용하냐면, user service 단독 unit test하고 싶기 때문이다. typeorm에서 real user를 불러오고 싶지 않다는 것이다. 불러오면 이건 unit test가 아니니께
    - provides -> useValue에 fake mock을 하나 만들어서 넣어버리자.

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -1,12 +1,51 @@
 import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
 import { UsersService } from './user.service';
+
+const mockRepository = {
+  findOne: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+  delete: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  sendVerificationEmail: jest.fn(),
+};
 
 describe('UserService', () => {
   let service: UsersService;
 
   beforeAll(async () => {
     const modules = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
     }).compile();
     service = modules.get<UsersService>(UsersService);
   });
