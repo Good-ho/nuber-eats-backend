767858d1302f267e0695159a7de34002fc36bab1
good-ho <whdxo100@gmail.com>
2021-01-20 20:50:15 +0900

    Mocking part Two
    - 테스트를 할때는 사고방식을 좀 바꿔야한다.
    - 우리는 먼저, createAccount 함수를 분리해서 테스트 할 것이다. 함수의 출력물이 아니라,  함수의 모든 부분을 테스트 할 것이다.
    - 먼저 findone같이 repository를 테스트 하기 위해,  userRepository변수 만들자.
    - 이 userRepository(mock repository)를 위한 Type을 만들어보자.
    - 즉, 실제 userRepository의 실제 모든 함수들을 모두 가져오겠다는 것. (findone, create, save등)
    - 이것을 위해 먼저 Parital 을 사용하자. Parital은 타입의 모든 요소를 optional하게 해주기 때문.
    - 그리고 record를 만들자. record는 타입 t의 k집합으로 타입을 만들어 주거든.
    	( const temp = Partial<Record<"hello", number>> 이렇게 작성하면 temp.hello 이 놈의 타입이 number로 된다는 것)
    
    - Partial<Record<keyof Repository<User>, jest Mock>>>
    - 위 처럼 작성하는 것의 의미는 user repository의 모든 Key를 jest mock 타입으로 만들겠다는 것이쥐.

--- src/users/user.service.spec.ts
+++ src/users/user.service.spec.ts
@@ -2,6 +2,7 @@ import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { JwtService } from 'src/jwt/jwt.service';
 import { MailService } from 'src/mail/mail.service';
+import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { Verification } from './entities/verification.entity';
 import { UsersService } from './user.service';
@@ -22,8 +23,11 @@ const mockMailService = {
   sendVerificationEmail: jest.fn(),
 };
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
 describe('UserService', () => {
   let service: UsersService;
+  let userRepository: MockRepository<User>;
 
   beforeAll(async () => {
     const modules = await Test.createTestingModule({
@@ -48,13 +52,16 @@ describe('UserService', () => {
       ],
     }).compile();
     service = modules.get<UsersService>(UsersService);
+    userRepository = modules.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    it('should fail if user exists', () => {});
+  });
   it.todo('login');
   it.todo('findById');
   it.todo('editProfile');
