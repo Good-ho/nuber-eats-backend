8d585011f391f726f33387a9cc5d328dc17649a3
good-ho <whdxo100@gmail.com>
2021-01-26 21:23:36 +0900

    sendVerificationEmail Test 1. spy 함수에 대한 이해 필요.       아래 expect(service.sendEmail) 구문에서 에러발생       왜냐면, sendEmail은 mock으로 되어있지 않기 때문.       우리는 마지막에 sendEmail에 대한 unit testing을 해야하므로 jest.fn() 이런 식으로 mock하면 문제생긴다.       여기서 새로운 개념은 spy 함수를 사용하자.       spyOn은 아래와 같이 원하는 함수를 가로채서 implementation할 수 있다.
          2. seendverificationemail 호출시 호출하는 sendemail에 전달하는 인자가 정상적으로 전달되는지 확인

--- src/mail/mail.service.spec.ts
+++ src/mail/mail.service.spec.ts
@@ -33,6 +33,38 @@ describe('MailService', () => {
     expect(service).toBeDefined();
   });
 
+  describe('sendVerificationEmail', () => {
+    it('should call sendEmail', () => {
+      const sendVerificationEmailArgs = {
+        email: 'email',
+        code: 'code',
+      };
+
+      // 아래 expect(service.sendEmail) 구문에서 에러발생
+      // 왜냐면, sendEmail은 mock으로 되어있지 않기 때문.
+      // 우리는 마지막에 sendEmail에 대한 unit testing을 해야하므로 jest.fn() 이런 식으로 mock하면 문제생긴다.
+      // 여기서 새로운 개념은 spy 함수를 사용하자.
+      // spyOn은 아래와 같이 원하는 함수를 가로채서 implementation할 수 있다.
+      jest.spyOn(service, 'sendEmail').mockImplementation(async () => {
+        // console.log('i love you');
+      });
+
+      service.sendVerificationEmail(
+        sendVerificationEmailArgs.email,
+        sendVerificationEmailArgs.code,
+      );
+
+      expect(service.sendEmail).toHaveBeenCalledTimes(1);
+      expect(service.sendEmail).toHaveBeenCalledWith(
+        'Verify your email',
+        'confirmemail',
+        [
+          { key: 'code', value: sendVerificationEmailArgs.code },
+          { key: 'username', value: sendVerificationEmailArgs.email },
+        ],
+      );
+    });
+  });
+
   it.todo('sendEmail');
-  it.todo('sendVerificationEmail');
 });
