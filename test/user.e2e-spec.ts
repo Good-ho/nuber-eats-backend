ad8eb2dab86f2c03f9318a9b1860f19835cd6309
good-ho <whdxo100@gmail.com>
2021-01-27 22:05:17 +0900

    Testing createAccount part Two
    warning이 아직 있다. --detectOpenHandles 이 조건으로 자세히 알아보라고 나온다.
    이걸 활용하기 위해 packages.json e2e test script 안에 저걸 넣자.
    
    적용 후 다시 돌려보면 got 모듈이 문제가 있다는 것을 알 수 있다.
    왜 이런일이 생기냐면, account를 생성할때마다 email을 전송하기 때문.
    따라서 got를 mock하자.
    
    다음으로 create account error testing을 하자

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -4,6 +4,8 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+jest.mock('got');
+
 const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('User Module (e2e)', () => {
@@ -49,7 +51,29 @@ describe('User Module (e2e)', () => {
         });
     });
 
-    it.todo('should fail if account already exists');
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation{
+            createAccount(input:{
+              email:"${EMAIL}",
+              password:"12345678",
+              role:Owner
+            }){
+              ok,
+              error
+            }
+          }`,
+        })
+        .expect(200)
+        .expect((res) => {
+          // console.log(res.body);
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual('already exists');
+        });
+    });
   });
   it.todo('userProfile');
   it.todo('login');
