6c739ecfa2a1f609149223f31c0811221b1d6c80
good-ho <whdxo100@gmail.com>
2021-01-30 22:23:18 +0900

    Search part Two
    이전 category할 때 custom category를 만들었다.
    pagination도 겁나 자주 사용되는데 repository로 사용하면 좋을거같다.(todo)
    일단 동작은 하나 문제점이 query 대소문자 구분을 하기 때문에 query search에 문제가 있음.
    이걸 해결하기 위해 typeorm이 제공하는 ILike라는 것을 사용해보자.
    ILike는 대소문자 구분안함.

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { EditProfileOutput } from 'src/users/dtos/edit-profile.dto';
 import { User } from 'src/users/entities/user.entity';
-import { Like, Repository } from 'typeorm';
+import { ILike, Like, Repository } from 'typeorm';
 import { AllCategoriesOutput } from './dtos/all-categories.dto';
 import { CategoryInput, CategoryOutput } from './dtos/category.dto';
 import {
@@ -254,9 +254,18 @@ export class RestaurantService {
     try {
       const [restaurants, totalResults] = await this.restaurants.findAndCount({
         where: {
-          name: Like(`%${query}%`),
+          name: ILike(`%${query}%`),
         },
+        take: 25,
+        skip: (page - 1) * 25,
       });
+
+      return {
+        ok: true,
+        restaurants,
+        totalResults,
+        totalPages: Math.ceil(totalResults / 25),
+      };
     } catch {
       return {
         ok: false,
