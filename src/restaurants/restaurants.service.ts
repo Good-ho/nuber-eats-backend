6f63aa3f2fbfd06c97b33958dfee0a20a96bfa8a
good-ho <whdxo100@gmail.com>
2021-01-30 11:31:07 +0900

    Edit Restaurant part Two
     - 문제점  : 어떤 restaurant을 수정해야하는지 알 수가 없다. (input에 id가 없기때문)
    - dto에 id 추가
    
    - editrestaurant service 작성
    - 먼저 수정하고 싶은 restaurant을 찾자.
    - findone에 loadrelationid 를 추가하자. restaurant에서 owner user id 를 찾아야하기 때문
    -  그런데 relation관계마다 loadrelation을 db에서 읽는것은 성능에 문제를 야기 할 수 있다.
    - 따라서 reationid 라는 것을 사용하자!

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { EditProfileOutput } from 'src/users/dtos/edit-profile.dto';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import {
   CreateRestaurantInput,
   CreateRestaurantOutput,
 } from './dtos/create-restaurant.dto';
+import { EditRestaurantInput } from './dtos/edit-restaurant.dto';
 import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurants.entity';
 
@@ -18,6 +20,19 @@ export class RestaurantService {
     private readonly categories: Repository<Category>,
   ) {}
 
+  async getOrCreateCategory(name: string): Promise<Category> {
+    const categoryName = name.trim().toLocaleLowerCase();
+
+    const categorySlug = categoryName.replace(/ /g, '-');
+    let category = await this.categories.findOne({ slug: categorySlug });
+    if (!category) {
+      category = await this.categories.save(
+        this.categories.create({ slug: categorySlug, name: categoryName }),
+      );
+    }
+    return category;
+  }
+
   async createRestaurant(
     owner: User,
     createRestaurantInput: CreateRestaurantInput,
@@ -25,17 +40,9 @@ export class RestaurantService {
     try {
       const newRestaurant = this.restaurants.create(createRestaurantInput);
       newRestaurant.owner = owner;
-      const categoryName = createRestaurantInput.categoryName
-        .trim()
-        .toLocaleLowerCase();
-
-      const categorySlug = categoryName.replace(/ /g, '-');
-      let category = await this.categories.findOne({ slug: categorySlug });
-      if (!category) {
-        category = await this.categories.save(
-          this.categories.create({ slug: categorySlug, name: categoryName }),
-        );
-      }
+      const category = await this.getOrCreateCategory(
+        createRestaurantInput.categoryName,
+      );
       newRestaurant.category = category;
       // repository create는 entity를 instance를 만드는 것이고 db에 저장하려면 save필요.
       await this.restaurants.save(newRestaurant);
@@ -46,6 +53,39 @@ export class RestaurantService {
       return {
         ok: false,
         error: 'could not create restaurant',
+      };
+    }
+  }
+
+  async editRestaurant(
+    owner: User,
+    editRestaurantInput: EditRestaurantInput,
+  ): Promise<EditProfileOutput> {
+    try {
+      const findRestaurant = await this.restaurants.findOne(
+        editRestaurantInput.restaurantID,
+      );
+      if (!findRestaurant) {
+        return {
+          ok: false,
+          error: 'Restaurant Not Found',
+        };
+      }
+
+      if (owner.id !== findRestaurant.ownerId) {
+        return {
+          ok: false,
+          error: "You Can't edit Restaurant that you don't own.",
+        };
+      }
+
+      return {
+        ok: true,
+      };
+    } catch (e) {
+      return {
+        ok: false,
+        error: 'Could Not Found Restaurant',
       };
     }
   }
