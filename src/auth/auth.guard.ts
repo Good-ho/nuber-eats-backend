42bb01b51dfa43aa8ea6884299a7f683eda5d821
good-ho <whdxo100@gmail.com>
2021-02-07 20:10:35 +0900

    Subscription Authentication part Two
    -jwt middleware에서 token을 받고 decode 후 user를 찾는 작업을 auth guard에서 수행하자.
    - auth deco를 보면 gqlContext['user'] 에서 user를 받고있다.
    - 따라서, guard가 먼저 호출되기 때문에, guard에서 gqlContext['user']에 decode한 user를 넣어주자

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -2,13 +2,19 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { isObject } from 'class-validator';
+import { JwtService } from 'src/jwt/jwt.service';
 import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/user.service';
 import { AllowedRoles } from './role.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
-  canActivate(context: ExecutionContext) {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {}
+  async canActivate(context: ExecutionContext) {
     // console.log(context);
     const roles = this.reflector.get<AllowedRoles>(
       'roles',
@@ -19,16 +25,28 @@ export class AuthGuard implements CanActivate {
       return true;
     }
     const gqlContext = GqlExecutionContext.create(context).getContext();
-    console.log(gqlContext.token);
-    const user: User = gqlContext['user'];
-    if (!user) {
+    const token = gqlContext.token;
+    if (token) {
+      const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        const { user } = await this.userService.findById(decoded['id']);
+
+        if (!user) {
+          return false;
+        }
+
+        gqlContext['user'] = user;
+
+        if (roles.includes('Any')) {
+          return true;
+        }
+
+        return roles.includes(user.role);
+      } else {
+        return false;
+      }
+    } else {
       return false;
     }
-
-    if (roles.includes('Any')) {
-      return true;
-    }
-
-    return roles.includes(user.role);
   }
 }
